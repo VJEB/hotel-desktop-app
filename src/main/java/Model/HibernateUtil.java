b2d7507d37927907da7c21533955529fab728996
VJEB <vespinozabenitez@gmail.com>
2023-08-15 21:45:20 -0600

    First commit 15-08-2023

---
+++ src/main/java/Model/HibernateUtil.java
@@ -0,0 +1,22 @@
+package Model;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        } catch (Throwable ex) {
+            // Handle initialization errors
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
+
