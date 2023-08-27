dbf8c84603794ea1bd461b60f33e9f4e56a5a3ea
VJEB <vespinozabenitez@gmail.com>
2023-08-27 16:57:36 -0600

    Tenth commit 27-08-2023

--- src/main/java/org/example/Controller/GuestView.java
+++ src/main/java/org/example/Controller/GuestView.java
@@ -111,7 +111,6 @@ public class GuestView {
         scaleTransition.setOnFinished(event -> modalStage.show());
         scaleTransition.play();
     }
-
     public void initialize() {
         initializeColumns();
         setupButtonCellFactory();
@@ -129,105 +128,116 @@ public class GuestView {
         nationalityColumn.setCellValueFactory(new PropertyValueFactory<>("nationality"));
     }
     private void setupButtonCellFactory() {
-        actionsColumn.setCellFactory(column -> new TableCell<Guest, Void>() {
-            private final JFXButton updateButton = new JFXButton();
-            private final JFXButton deleteButton = new JFXButton();
-
-            {
-                ImageView editIcon = new ImageView(new Image(Objects.requireNonNull(Main.class.getResourceAsStream("../../Icons/pencil.png"))));
-                editIcon.setFitWidth(15);
-                editIcon.setFitHeight(15);
-                updateButton.getStyleClass().add("edit-button");
-                updateButton.setGraphic(editIcon);
-
-                ImageView binIcon = new ImageView(new Image(Objects.requireNonNull(Main.class.getResourceAsStream("../../Icons/trash-bin.png"))));
-                binIcon.setFitWidth(15);
-                binIcon.setFitHeight(15);
-                deleteButton.getStyleClass().add("delete-button");
-                deleteButton.setGraphic(binIcon);
-
-                updateButton.setOnAction(event -> {
-                    Guest guest = getTableRow().getItem();
-                    guestToBeUpdated = guest;
-
-                    if (guest != null) {
-                        try {
-                            FXMLLoader loader = new FXMLLoader(Main.class.getResource("../../Forms/guestForm.fxml"));
-                            Parent form = loader.load();
-                            Image hotelIcon = new Image(Objects.requireNonNull(Main.class.getResourceAsStream("../../Images/hotel.png")));
-
-                            Stage modalStage = new Stage();
-                            modalStage.initModality(Modality.APPLICATION_MODAL);
-                            modalStage.getIcons().add(hotelIcon);
-
-                            ScaleTransition scaleTransition = new ScaleTransition(Duration.seconds(0.2), form);
-                            scaleTransition.setFromX(0.5);
-                            scaleTransition.setFromY(0.5);
-                            scaleTransition.setToX(1);
-                            scaleTransition.setToY(1);
-
-                            Scene scene = new Scene(form);
-                            modalStage.setScene(scene);
-
-                            modalStage.show();
-                            scaleTransition.play();
-
-                            GuestForm guestFormController = loader.getController();
-                            guestFormController.setGuestToUpdate(guest);
-                            guestFormController.setStage(modalStage);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-                deleteButton.setOnAction(event -> {
-                    Guest guest = getTableRow().getItem();
-                    if (guest != null) {
-                        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-                        alert.setTitle("Confirm Deletion");
-                        alert.setHeaderText("Are you sure you want to delete the guest: " + guest.getFirstName() + " " + guest.getLastName() + "?");
-
-                        DialogPane dialogPane = alert.getDialogPane();
-                        dialogPane.getStylesheets().add(
-                                Objects.requireNonNull(Main.class.getResource("../../application.css")).toExternalForm());
-                        dialogPane.getStyleClass().add("myDialog");
-                        ImageView warningIon = new ImageView(new Image(Objects.requireNonNull(Main.class.getResourceAsStream("../../Icons/warning.png"))));
-                        warningIon.setFitHeight(48);
-                        warningIon.setFitWidth(48);
-                        dialogPane.setGraphic(warningIon);
-
-                        Optional<ButtonType> result = alert.showAndWait();
-                        if (result.isPresent() && result.get() == ButtonType.OK) {
-                            guestsObservableList.remove(guest);
-                            GuestDAO.removeGuestFromCachedGuests(guest);
-                            if (new GuestDAO().deleteGuest(guest.getNational_id())) {
-                                System.out.println("Guest deleted successfully!");
-                            } else {
-                                System.out.println("There was an error deleting the guest " + guest.getFirstName() + " with national id of: " + guest.getNational_id());
-                            }
-                        }
-                    }
-                });
-            }
-
-            @Override
-            protected void updateItem(Void item, boolean empty) {
-                super.updateItem(item, empty);
-
-                if (empty) {
-                    setGraphic(null);
-                    System.out.println("updateItem Override: WITHOUT BUTTONS CELL");
-                } else {
-                    HBox buttonContainer = new HBox(updateButton, deleteButton);
-                    buttonContainer.setAlignment(Pos.CENTER);
-                    buttonContainer.setSpacing(10);
-                    setGraphic(buttonContainer);
-                    System.out.println("updateItem Override: BUTTONS CELL");
-                }
-            }
-        });
-
+        actionsColumn.setCellFactory(column -> new ButtonCell());
     }
+
+    private class ButtonCell extends TableCell<Guest, Void> {
+        private final JFXButton updateButton = new JFXButton();
+        private final JFXButton deleteButton = new JFXButton();
+
+        public ButtonCell() {
+            setupUpdateButton();
+            setupDeleteButton();
+        }
+
+        private void setupUpdateButton() {
+            // Set up update button properties, styles, and actions
+            updateButton.getStyleClass().add("edit-button");
+            ImageView pencilImageContainer = new ImageView(new Image(Objects.requireNonNull(Main.class.getResourceAsStream("../../Icons/pencil.png"))));
+            pencilImageContainer.setFitWidth(15);
+            pencilImageContainer.setFitHeight(15);
+            updateButton.setGraphic(pencilImageContainer);
+            updateButton.setOnAction(event -> handleUpdateButtonClick());
+        }
+
+        private void setupDeleteButton() {
+            // Set up delete button properties, styles, and actions
+            deleteButton.getStyleClass().add("delete-button");
+            ImageView binImageContainer = new ImageView(new Image(Objects.requireNonNull(Main.class.getResourceAsStream("../../Icons/trash-bin.png"))));
+            binImageContainer.setFitWidth(15);
+            binImageContainer.setFitHeight(15);
+            deleteButton.setGraphic(binImageContainer);
+            deleteButton.setOnAction(event -> handleDeleteButtonClick());
+        }
+
+        private void handleUpdateButtonClick() {
+            Guest guest = getTableRow().getItem();
+            guestToBeUpdated = guest;
+
+            if (guest != null) {
+                openGuestFormModal(guest);
+            }
+        }
+
+        private void handleDeleteButtonClick() {
+            Guest guest = getTableRow().getItem();
+            if (guest != null) {
+                showDeleteConfirmationAlert(guest);
+            }
+        }
+
+        @Override
+        protected void updateItem(Void item, boolean empty) {
+            super.updateItem(item, empty);
+
+            if (empty) {
+                setGraphic(null);
+                System.out.println("updateItem Override: WITHOUT BUTTONS CELL");
+            } else {
+                HBox buttonContainer = new HBox(updateButton, deleteButton);
+                buttonContainer.setAlignment(Pos.CENTER);
+                buttonContainer.setSpacing(10);
+                setGraphic(buttonContainer);
+                System.out.println("updateItem Override: BUTTONS CELL");
+            }
+        }
+    }
+
+    private void openGuestFormModal(Guest guest) {
+        try {
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("../../Forms/guestForm.fxml"));
+            Parent form = loader.load();
+            Stage modalStage = createModalStage();
+
+            setupModalAnimation(modalStage, form);
+
+            GuestForm guestFormController = loader.getController();
+            guestFormController.setGuestToUpdate(guest);
+            guestFormController.setStage(modalStage);
+
+            modalStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showDeleteConfirmationAlert(Guest guest) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirm Deletion");
+        alert.setHeaderText("Are you sure you want to delete the guest: " + guest.getFirstName() + " " + guest.getLastName() + "?");
+
+        DialogPane dialogPane = alert.getDialogPane();
+        dialogPane.getStylesheets().add(
+                Objects.requireNonNull(Main.class.getResource("../../application.css")).toExternalForm());
+        dialogPane.getStyleClass().add("myDialog");
+        dialogPane.setGraphic(new ImageView(new Image(Objects.requireNonNull(Main.class.getResourceAsStream("../../Icons/warning.png")))));
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            handleGuestDeletion(guest);
+        }
+    }
+
+    private void handleGuestDeletion(Guest guest) {
+        guestsObservableList.remove(guest);
+        GuestDAO.removeGuestFromCachedGuests(guest);
+        if (new GuestDAO().deleteGuest(guest.getNational_id())) {
+            System.out.println("Guest deleted successfully!");
+        } else {
+            System.out.println("There was an error deleting the guest " + guest.getFirstName() + " with national id of: " + guest.getNational_id());
+        }
+    }
+
     private void loadGuestData() {
         List<Guest> cachedGuests = GuestDAO.getCachedGuests();
         if (cachedGuests == null) {
@@ -281,7 +291,7 @@ public class GuestView {
         String addedGuestNationalId = addedGuest.getNational_id();
         int addedItems = change.getAddedSize();
         if (addedItems == 1) {
-            if (Objects.equals(addedGuestNationalId, guestToBeUpdated.getNational_id())) {
+            if (guestToBeUpdated != null && Objects.equals(addedGuestNationalId, guestToBeUpdated.getNational_id())) {
                 FormHelper.showSnackbar(borderPane, "Guest updated!");
             } else {
                 FormHelper.showSnackbar(borderPane, "Guest added!");
@@ -295,10 +305,10 @@ public class GuestView {
         String removedGuestNationalId = removedGuest.getNational_id();
         int removedItems = change.getRemovedSize();
         if (removedItems == 1) {
-            if (!Objects.equals(removedGuestNationalId, guestToBeUpdated.getNational_id())) {
+            if (guestToBeUpdated != null && !Objects.equals(removedGuestNationalId, guestToBeUpdated.getNational_id())) {
                 FormHelper.showSnackbar(borderPane, "Guest removed!");
-                filteredTableView.refresh();
             }
+            filteredTableView.refresh();
         }
     }
 
